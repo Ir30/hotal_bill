d7f69825cefe337f3a5e9476f2c285dfd6c5d2d0
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-22 12:50:50 +0530

    Development

--- Hotal.java
+++ Hotal.java
@@ -15,5 +15,39 @@ class hotel{
     float tea_total;
     float snak_total;
     float cofee_total;
+    hotel(String name,long phone){
+        this.name=name;
+        this.phone = phone;
+    }
 
+    void generate_bill(){
+        tea_total = tea*tea_q;
+        snak_total = snak*snak_q;
+        cofee_total = cofee*cofee_q;
+    }
+
+    void display_bill(){
+        System.out.println();
+        System.out.println("Total Bil");
+        System.out.println("-------------------");
+        System.out.println();
+
+        System.out.println("name:"+name);
+        System.out.println("1.cofee  "+cofee_q+"    "+ cofee_total);
+        System.out.println("1.Tea  "+tea_q+"    "+ tea_total);
+        System.out.println("1.snak  "+snak_q+"    "+ snak_total);
+        System.out.println();
+        System.out.println("-------------------");
+
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<hotel> customer_list = new ArrayList<hotel>();
+
+
+    }
 }
