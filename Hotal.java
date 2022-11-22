b152777604e8588aabf9ab98b3f1dee48fbc98b7
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-22 12:42:57 +0530

    starting

---
+++ Hotal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+class hotel{
+    String name;
+    long phone;
+
+    float tea=10;
+    float snak=10;
+    float cofee = 15;
+    
+    int tea_q=0;
+    int snak_q=0;
+    int cofee_q=0;
+
+    float tea_total;
+    float snak_total;
+    float cofee_total;
+
+}
